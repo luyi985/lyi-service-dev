3d8262c5a21f5b5eb1ffae3e4bb7c72db0c028ea
LuYi <luyi985@gmail.com>
2018-07-26 23:57:30 +1000

    write bin

--- bin/bin.js
+++ bin/bin.js
@@ -1,1 +1,56 @@
 #!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+const { exec } = require('child_process')
+const srcPath = path.join(__dirname, '../lyi-service-dev');
+const targetPath = process.cwd()
+
+const fileToCopy = ['.dockfile', '.eslintrc.js', '.prettierrc', 'jest.config.js'];
+
+function copyFiles() {
+    fileToCopy.forEach(file => {
+        fs.copyFileSync(`${srcPath}/${file}`,`${targetPath}/${file}`);
+    });
+}
+
+function updatePackageJson(error, stdout, stderr) {
+    
+    try {
+        const srcJson = require(`${srcPath}/package.json`);
+        const targetJson = require(`${targetPath}/package.json`);
+
+        if(!srcJson || !targetJson) {
+            throw new Error('Can not find package.json')
+        }
+
+        const { 
+            dependencies,
+            main, 
+            scripts, 
+            devDependencies
+        } = srcJson;
+
+        const resultJson = Object.assign({}, targetJson, {dependencies, main, scripts, devDependencies}, {'lint-staged': srcJson['lint-staged']});
+        fs.writeFileSync(`${targetPath}/package.json`, JSON.stringify(resultJson));
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+function npmInit(callBack) {
+    if (fs.existsSync(`${targetPath}/package.json`)) {
+        callBack();
+        return;
+    }    
+    exec('npm init -y', callBack);
+}
+
+copyFiles();
+npmInit(updatePackageJson)
+exec(`cp -rv ${srcPath}/app ${targetPath}/app`);
+exec(`cp -rv ${srcPath}/bin ${targetPath}/bin`);
+exec(`cp -rv ${srcPath}/config ${targetPath}/config`);
+exec(`cp -rv ${srcPath}/__test__ ${targetPath}/__test__`);
+console.log('-----Project has been setted up-------')
+console.log('Installing Package...')
+exec('npm i');
